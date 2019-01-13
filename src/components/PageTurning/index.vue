81f3c579baa2c2cecc9942b32dc4838be35726d7
houzhiqiang <zhq.hou@muheda.com>
2019-01-14 00:16:00 +0800

    PageTurning计算属性

--- src/components/PageTurning/index.vue
+++ src/components/PageTurning/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="PageTurning">
     <div class="flex row justify-end align-center">
-      <span class="mr20">第{{ page }}/{{ Math.ceil(pageTotal/pageSize) }}页</span>
+      <span class="mr20">第{{ page }}/{{ lastPage }}页</span>
       <Button class="mr20" :disabled="page===1" @click="handleFirstPage">首页</Button>
       <Page :current="page" :total="pageTotal" :page-size="pageSize" @on-change="pageChange" />
-      <Button class="ml20" :disabled="page===Math.ceil(pageTotal/pageSize)" @click="handleLastPage">尾页</Button>
+      <Button class="ml20" :disabled="page===lastPage" @click="handleLastPage">尾页</Button>
     </div>
   </div>
 </template>
--- src/components/PageTurning/src/index.js
+++ src/components/PageTurning/src/index.js
@@ -22,15 +22,19 @@ export default {
       page: 1
     }
   },
+  computed: {
+    lastPage () {
+      return Math.ceil(this.pageTotal / this.pageSize)
+    }
+  },
   methods: {
     handleFirstPage () {
       this.page = 1
       this.$emit('on-page-change', 1)
     },
     handleLastPage () {
-      let lastIndex = Math.ceil(this.pageTotal / this.pageSize)
-      this.page = lastIndex
-      this.$emit('on-page-change', lastIndex)
+      this.page = this.lastPage
+      this.$emit('on-page-change', this.lastPage)
     },
     pageChange (index) {
       this.page = index
